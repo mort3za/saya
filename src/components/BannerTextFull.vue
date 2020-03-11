ec69bc9962dc7040df9d01d2fe9c51cb80d11a12
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-03-11 12:12:33 +0330

    add aos animations

--- src/components/BannerTextFull.vue
+++ src/components/BannerTextFull.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="primary em-high">
-    <v-container class="py-9">
+    <v-container class="py-9" data-aos="fade-up">
       <v-row no-gutters="">
         <v-col cols="8">
           <h2 class="display-3 mb-0 text-transform-uppercase" v-html="title"></h2>
@@ -16,7 +16,3 @@ export default Vue.extend({
   props: ['title']
 });
 </script>
-
-<style lang="scss" scoped>
-//
-</style>
