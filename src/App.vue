8e5575288653b20d91f1fe6529ec6a956b4dfa72
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-03-11 17:24:16 +0330

    splash screen on loading

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,20 @@
   <v-app class="wrapper" id="app">
     <router-view />
     <!-- <page-progress-bar-global v-if="shouldShowProgressBar"></page-progress-bar-global> -->
+    <transition name="fade">
+      <page-splash-screen v-if="isSplashScreenVisible"></page-splash-screen>
+    </transition>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import { appTitle } from '@/constants/app';
+import PageSplashScreen from '@/components/PageSplashScreen.vue';
 
 export default Vue.extend({
   name: 'app',
+  components: { PageSplashScreen },
   metaInfo: {
     title: appTitle
   },
